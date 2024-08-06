c8d3a52efde4cb2f450768881346accf1a8152b0
cheol <qpsaone2@naver.com>
2024-08-06 01:08:44 +0000

    create project

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class DemoController {
+    @GetMapping("/test")
+    public String test() {
+        return "test demo22";
+    }
+}
